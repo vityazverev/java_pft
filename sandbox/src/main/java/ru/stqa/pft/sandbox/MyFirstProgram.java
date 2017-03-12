5c90b2a40dc6278d0cb15656db900674fd13296f
vityazverev <vityazverev@gmail.com>
2017-03-12 22:19:47 +0200

    Создание классов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,28 +2,27 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
-        hello("world");
-        hello("user");
-        hello("Victor");
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Victor");
 
-        double l = 5;
-      System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-      double a = 4;
-      double b = 6;
-      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
-      }
-  
-    public static void hello(String somebody) {
-      System.out.println("Hello, " + somebody + "!");
-    }
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+  }
 
-    public static double area (double len){
-      return len * len;
-    }
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
 
-    public static double area (double a, double b){
-      return a * b;
-    }
+  public static double area(Square s){
+    return s.l * s.l;
+  }
+
+  public static double area(Rectangle r){
+    return r.a * r.b;
+  }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Виктор on 12.03.2017.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+}
