3cebecaa8807acbc91a53b1a40d07095e33e4aa6
vityazverev <vityazverev@gmail.com>
2017-03-15 10:16:21 +0200

    Задание 2.2

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Task2_3.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Виктор on 15.03.2017.
+ */
+public class Task2_3 {
+  public static void main(String[] args){
+    double x1 = 2;
+    double y1 = 2;
+    double x2 = 5;
+    double y2 = 5;
+    System.out.println("Длина от точни P1 до точки P2" + " = "  + Math.sqrt((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2)));
+  }
+
+}
