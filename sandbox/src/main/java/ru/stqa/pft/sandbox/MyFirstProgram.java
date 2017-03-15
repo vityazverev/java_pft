b6621bd107fb832cf9e72bc64a8a6dc1754d89af
vityazverev <vityazverev@gmail.com>
2017-03-15 10:04:31 +0200

    Удаление новых тестовых модулей

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,9 +12,28 @@ public class MyFirstProgram {
 
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    double x1 = 2;
+    double y1 = 2;
+    double x2 = 5;
+    double y2 = 5;
+    System.out.println("Длина от точни P1 до точки P2" + " = "  + Math.sqrt((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2)));
+
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
+
   }
-}
+
+  }
+ /* public static void distance() {
+    double x1 = 2;
+    double y1 = 2;
+    double x2 = 5;
+    double y2 = 5;
+    System.out.println("Длина от точни P1 до точки P2" + " = "  + ((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2)));
+    }*/
+
+
+
