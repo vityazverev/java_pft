33d6b4f3f9e064a898365259ceb5261d4462fa36
vityazverev <vityazverev@gmail.com>
2017-03-16 14:59:12 +0200

    конечный результат

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
@@ -6,9 +6,17 @@ package ru.stqa.pft.sandbox;
 public class Task2 {
 
   public static void main(String[] args) {
-    Point p1 = new Point(1, 2);
+    Point p1 = new Point(1, 1);
     Point p2 = new Point(5, 6);
+   /* Point p1 = new Point();
+    p1.x = 1;
+    p1.y = 1;
+    Point p2 = new Point();
+    p2.x = 5;
+    p2.y = 6;*/
     System.out.println("Расстояние между двумя точками = " + distance(p1, p2));
+
+
   }
 
 
