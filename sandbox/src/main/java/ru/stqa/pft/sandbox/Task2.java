fb2bf79c0ace73681a129c9b90a27dc37277dd0e
vityazverev <vityazverev@gmail.com>
2017-03-17 20:14:39 +0200

    Тест для веб-приложения

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
@@ -14,14 +14,14 @@ public class Task2 {
     Point p2 = new Point();
     p2.x = 5;
     p2.y = 6;*/
-    System.out.println("Расстояние между двумя точками = " + distance(p1, p2));
+    System.out.println("Расстояние между двумя точками " + p1.distance(p2) + "= " + distance(p1, p2));
 
 
   }
 
 
     public static double distance(Point p1, Point p2){
-      return Math.sqrt(( p1.x - p2.x) * ( p1.x - p2.x) + ( p1.x - p2.x) * ( p1.x -  p2.x));
+      return Math.sqrt(( p1.x - p2.x) * ( p1.x - p2.x) + ( p1.y - p2.y) * ( p1.y -  p2.y));
     }
 
 }
