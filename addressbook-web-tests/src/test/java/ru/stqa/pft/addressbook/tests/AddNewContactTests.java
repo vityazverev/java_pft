b93e7c1d0df12d6b0c862835e25f7a28beba5a9d
vityazverev <vityazverev@gmail.com>
2017-03-27 13:05:36 +0300

    Тест для создания Контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddNewContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddNewContactTests.java
@@ -5,26 +5,14 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 
-public class AddNewContact extends TestBase {
+public class AddNewContactTests extends TestBase {
 
   @Test
   public void testAddNewContactCreation() {
     app.getNavigationHelper().goToAddNewContactPage();
     app.getContatHelper().fillContactForm(new ContactData("Viktor", "Zverev", "Odessa", "0938478961", "0932222222", "vityazverev@gmail.com"));
-
-  }
-
-
-
-public class GroupCreationTests extends TestBase {
-
-  @Test
-  public void testGroupGreation() {
-    app.getNavigationHelper().goToGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    app.getContatHelper().submitContactCreation();
+    app.getNavigationHelper().goToHomePage();
   }
 
 }
