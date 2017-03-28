f9d0fa209747bea3756931b0fd11b4c9a72bdd37
vityazverev <vityazverev@gmail.com>
2017-03-28 12:09:29 +0300

    Функция и тест Equation

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -0,0 +1,27 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/*Created by Виктор on 28.03.2017.*/
+
+public class EquationTests {
+
+  @Test
+  public void test0() {
+    Equation e = new Equation (1, 1, 1);
+    Assert.assertEquals(e.rootNumbers(), 0);
+  }
+
+  @Test
+  public void test1() {
+    Equation e = new Equation (1, 2, 1);
+    Assert.assertEquals(e.rootNumbers(), 1);
+  }
+
+  @Test
+  public void test2() {
+    Equation e = new Equation (1, 5, 6);
+    Assert.assertEquals(e.rootNumbers(), 2);
+  }
+}
