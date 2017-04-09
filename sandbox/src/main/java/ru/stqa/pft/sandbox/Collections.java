5dbf27e80cf5483d68f04d23bff86f91477b7cf5
vityazverev <vityazverev@gmail.com>
2017-04-09 16:32:18 +0300

    Колекции

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Виктор on 09.04.2017.
+ */
+
+
+public class Collections {
+
+  public static void main(String[] args){
+    String[] langs = {"Java", "C#", "Pyton", "PHP"};
+
+    for (int i = 0; i < langs.length; i++){
+      System.out.println("Я хочу выучить " + langs[i]);
+    }
+  }
+}
