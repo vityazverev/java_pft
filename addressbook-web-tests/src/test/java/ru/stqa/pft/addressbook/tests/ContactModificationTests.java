b625088265f93cce930869ca19fb8f853f7a3641
vityazverev <vityazverev@gmail.com>
2017-04-10 13:07:31 +0300

    Выбор группы по порядковому номеру

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -10,7 +10,9 @@ public class ContactModificationTests extends TestBase{
   @Test
   public void testContactModification(){
     app.getNavigationHelper().goToHomePage();
+/*
     int before = app.getGroupHelper().getGroupCount();
+*/
     if (! app.getContatHelper().isThereAContact() ) {
       app.getContatHelper().createContact(new ContactData("Viktor", "Zverev", "Odessa",
               "0938478961", "0932222222", "vityazverev@gmail.com", "test1"), true);
@@ -20,7 +22,7 @@ public class ContactModificationTests extends TestBase{
             "0938478961", "0932222222", "vityazverev@gmail.com", "null"), false);
     app.getContatHelper().submitContactModification();
     app.getNavigationHelper().goToHomePage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
+    /*int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before);*/
   }
 }
