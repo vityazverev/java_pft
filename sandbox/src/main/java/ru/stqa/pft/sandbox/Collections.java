c41afa0db4704f235da040f2a73d3e78d36f90c7
vityazverev <vityazverev@gmail.com>
2017-04-10 09:53:41 +0300

    Коллекции, использование List

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Виктор on 09.04.2017.
  */
@@ -8,9 +11,14 @@ package ru.stqa.pft.sandbox;
 public class Collections {
 
   public static void main(String[] args){
-    String[] langs = {"Java", "C#", "Pyton", "PHP"};
+    String[] langs = {"Java", "C#", "Python", "PHP"};
 
-    for (String l : langs){
+    List<String> languages = new ArrayList<String>();
+    languages.add("Java");
+    languages.add("C#");
+    languages.add("Python");
+
+    for (String l : languages){
       System.out.println("Я хочу выучить " + l);
     }
   }
