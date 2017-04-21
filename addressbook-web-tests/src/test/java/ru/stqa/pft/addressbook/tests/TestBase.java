c452a5f54b55848ce940c1b6bf99daa7e69fdfd6
vityazverev <vityazverev@gmail.com>
2017-04-21 18:45:00 +0300

    l5 m1 Launch TestSuite

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,10 +1,9 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 /**
  * Created by Виктор on 21.03.2017.
@@ -12,14 +11,14 @@ import org.openqa.selenium.remote.BrowserType;
 public class TestBase {
 
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-  @BeforeMethod
+  @BeforeSuite
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterMethod
+  @AfterSuite
   public void tearDown() {
     app.stop();
   }
