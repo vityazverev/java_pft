8554453433e47b467fe7ce1aad2e2f213158cc1e
vityazverev <vityazverev@gmail.com>
2017-05-01 20:31:00 +0300

    Rename class AddNewContact to ContactCreation

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,86 +1,42 @@
-/*package ru.stqa.pft.addressbook.tests;
+package ru.stqa.pft.addressbook.tests;
 
-
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactCreationTests {
-  FirefoxDriver wd;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 
-  @BeforeMethod
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 
-    wd.get("http://localhost/addressbook/");
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-  }
+public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
-    goToAddNewContactPage();
-    fillContactForm(new ContactData("Viktor", "Zverev", "Odessa", "0938478961", "0932222222", "vityazverev@gmail.com"));
-    submitContactCreation();
-    goToHomePage();
-  }
+    app.getNavigationHelper().goToHomePage();
+    List<ContactData> before = app.getContatHelper().getContactList();
+    app.getNavigationHelper().goToAddNewContactPage();
+    ContactData contact = new ContactData("Viktor", "Zverev", null,
+            null, null, null, "test1");
+    app.getContatHelper().createContact(contact, true);
+    List<ContactData> after = app.getContatHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() + 1);
 
-  private void goToHomePage() {
-    wd.findElement(By.linkText("home")).click();
-  }
-
-  private void submitContactCreation() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-  }
-
-  private void fillContactForm(ContactData contactData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobilephone());
-    wd.findElement(By.name("work")).click();
-    wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys(contactData.getWorkphone());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-  }
-
-  private void goToAddNewContactPage() {
-    wd.findElement(By.linkText("add new")).click();
-  }
-
-  @AfterMethod
-  public void tearDown() {
-    wd.quit();
-  }
-
-  public static boolean isAlertPresent(FirefoxDriver wd) {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+/*
+    int max = 0;
+    for (ContactData g : after) {
+      if (g.getId() > max) {
+        max = g.getId();
+      }
     }
+    contact.setId(max);
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));*/
+
+    before.add(contact);
+    Comparator<? super ContactData> byId =  (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
-}*/
+}
